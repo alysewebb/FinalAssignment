0168c8d9c14af3e3f2383469853676bbf40fa55b
Graphic <graphic@SOD-SJH-516-08M.local>
2015-11-20 14:38:19 -0500

    added rotate plugin and got the handle to rotate on mousedown. Stops on mouseout and mouseup.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var angle = 0;
+var animation;
 
+var rotate = function(){
+    angle+=3;
+	$(".handle").rotate(angle);
+};
+
+$(".handle").mousedown(function() {	
+	animation = setInterval(rotate,20);
+});
+
+$(".handle").mouseout(function() {	
+	clearInterval(animation);
+});
+
+$(".handle").mouseup(function() {	
+	clearInterval(animation);
+});
