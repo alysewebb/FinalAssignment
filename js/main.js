baa6fd882cfe32603cf2ca59e72203e7d9c2c5b8
Alyse Webb <alyse.webb@zemind.ca>
2015-12-04 09:37:49 -0500

    Set first cog to rotate when it is on the correct peg and the handle is turned

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 var angle = 0;
 var animation;
 
+$(".cog3").data('correctPeg', $(".peg1"));
+
 var rotate = function(){
     angle+=3;
 	$(".handle").rotate(angle);
+
+	// Adapted from:
+	//  http://stackoverflow.com/questions/5177867/how-to-find-out-about-the-snapped-to-element-for-jquery-ui-draggable-elements
+	var cog3 = $(".cog3");
+
+	/* Get all possible elements we can snap to */
+	var snappables = cog3.data('ui-draggable').snapElements;
+    var correctPeg = cog3.data('correctPeg')[0];
+
+    // Look through snap targets for one which is 'snapping' and is also the correctPeg
+  	var snappedToCorrectPeg = false;
+  	$.each(snappables, function (index, element) {
+  		if (element.snapping && correctPeg == element.item) {
+  			snappedToCorrectPeg = true;
+  		}
+  	});
+
+  	//and rotate it
+  	if (snappedToCorrectPeg) {
+		cog3.rotate(-angle);
+    }
 };
 
 //Make the handle cog turn when you hold 
@@ -33,15 +56,16 @@ $(".peg").droppable(
 
 $(".cog").draggable({ snap: ".peg", snapMode: "inner", 
 	containment: "parent", revert: false,
-	start: function(event, ui) {
-		ui.helper.data('dropped', false);
-	},
-	stop: function(event, ui) {
+	// start: function(event, ui) {
+	// 	ui.helper.data('dropped', false);
+	// },
+	// stop: function(event, ui) {
 
-		ui.draggable.css({bottom:'originalBottom', left:'originalLeft'})
-	}
+	// 	ui.draggable.css({bottom:'originalBottom', left:'originalLeft'})
+	// }
 
 });
+
 
 
 
